c21f32ad01c65e32201b3f7eb32d92092f4cd323
chaitanya9545 <57949027+chaitanya9545@users.noreply.github.com>
2023-03-08 14:40:46 +0530

    Update firstpage.dart

--- firstpage.dart
+++ firstpage.dart
@@ -1,48 +1,100 @@
-import 'package:cppmobile/widgets/alarmpage.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
 
-class Firstpage extends StatelessWidget {
-  const Firstpage({super.key});
+// ignore: camel_case_types
+class alarmpage extends StatefulWidget {
+  const alarmpage({super.key});
+
+  @override
+  State<alarmpage> createState() => _alarmpageState();
+}
+
+// ignore: camel_case_types
+class _alarmpageState extends State<alarmpage> {
+  bool isOn = false;
+  int alarmId = 1;
+
+  TimeOfDay _timeOfDay = const TimeOfDay(hour: 8, minute: 30);
+
+  // show time picker method
+  void _showTimePicker() {
+    showTimePicker(
+      context: context,
+      initialTime: TimeOfDay.now(),
+    ).then((timevalue) {
+      setState(() {
+        _timeOfDay = timevalue!;
+      });
+    });
+  }
+
+  void firealarm() {
+    // ignore: avoid_print
+    print("Alarm Fired at${DateTime.now()}");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Center(
+    return Column(
+      children: [
+        Container(
+          width: 200,
+          height: 200,
+          decoration: BoxDecoration(
+            border: Border.all(
+              width: 2,
+              color: Colors.red,
+            ),
+          ),
           child: Center(
-            child: Column(
-              children: const [
-                alarmpage(),
-              ],
+            child: Transform.scale(
+              scale: 2,
+              child: Switch(
+                value: isOn,
+                onChanged: (value) {
+                  setState(() {
+                    isOn = value;
+                  });
+                  if (isOn == true) {
+                    AndroidAlarmManager.oneShotAt(
+                        DateTime.now(), alarmId, firealarm);
+                  } else {
+                    AndroidAlarmManager.cancel(alarmId);
+                  }
+                },
+              ),
             ),
           ),
         ),
-      ),
-      drawer: Drawer(
-        child: ListView(
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text('Drawer Header'),
+        Container(
+          color: Colors.red,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                // display the chosen time
+                Text(
+                  _timeOfDay.format(context).toString(),
+                  style: const TextStyle(fontSize: 50),
+                ),
+
+                // button
+                MaterialButton(
+                  onPressed: _showTimePicker,
+                  color: Colors.blue,
+                  child: const Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      'PICK TIME',
+                      style: TextStyle(color: Colors.white, fontSize: 30),
+                    ),
+                  ),
+                ),
+              ],
             ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
   }
 }
